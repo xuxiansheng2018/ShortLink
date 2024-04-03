aaab1ada231a6417db130262c6df3ef11cedbf63
dzf <2359458679@qq.com>
2024-04-03 13:36:53 +0800

    ShortLink短链接项目启动

---
+++ admin/src/main/java/com/xgz/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package com.xgz.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author xuguozhu
+ * @Create 2024/4/3 13:26
+ * Level:
+ * Description:
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
