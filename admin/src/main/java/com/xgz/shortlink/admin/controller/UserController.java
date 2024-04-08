2c28cf1c98dc6d0cd244b347a4722fb83af4338b
xuxiansheng2018 <3306078606@qq.com>
2024-04-08 16:51:55 +0800

    查询 用户名是否存在 存在问题  当海量用户查询的时候 数据库承受不了

--- admin/src/main/java/com/xgz/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/xgz/shortlink/admin/controller/UserController.java
@@ -7,6 +7,7 @@ import com.xgz.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,23 +20,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
     /**
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserByUserName(username));
+        //之前的代码版本
 //        UserRespDTO result = userService.getUserByUserName(username);
-
-
 //        if (result == null) {
 //            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
 //        } else {
 ////            return new Result<UserRespDTO>().setCode("0").setData(result);  Result-->Results
 //            return Results.success(result);
 //        }
+    }
 
-        return Results.success(userService.getUserByUserName(username));
-
+    /**
+     * 查询用户名是否存在
+     */
+    @GetMapping("/api/shortlink/v1/user/has-username")
+    public Result<Boolean> hasUserName(@RequestParam("username") String username) {
+        Boolean b = userService.hasUsername(username);
+        return Results.success(b);
     }
 }
 
