eb2077727648e3d7eafa471994090f9bdb429c98
xuxiansheng2018 <3306078606@qq.com>
2024-04-08 16:05:13 +0800

    全局异常设计

---
+++ admin/src/main/java/com/xgz/shortlink/admin/common/convention/exception/ClientException.java
@@ -0,0 +1,42 @@
+package com.xgz.shortlink.admin.common.convention.exception;
+
+/**
+ * @Author xuguozhu
+ * @Create 2024/4/8 14:54
+ * Level:
+ * Description:
+ */
+
+
+import com.xgz.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import com.xgz.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * 客户端异常
+ */
+public class ClientException extends AbstractException {
+
+    public ClientException(IErrorCode errorCode) {
+        this(null, null, errorCode);
+    }
+
+    public ClientException(String message) {
+        this(message, null, BaseErrorCode.CLIENT_ERROR);
+    }
+
+    public ClientException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ClientException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ClientException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
